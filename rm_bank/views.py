133b3b8e7fdad10669a2187cd8ed486e37a4eb69
Abdul Muiz <abdulmuizpk@gmail.com>
2025-05-26 03:10:51 +0500

    Project configuration

--- rm_bank/views.py
+++ rm_bank/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-def index(req):
-  return HttpResponse("Hello")
+@api_view(['GET'])
+def index(request):
+    return Response("Hello World!")
