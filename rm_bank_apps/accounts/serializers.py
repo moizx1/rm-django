49addcd8b63bed0ea7357995ba7b462a8f352f0e
Abdul Muiz <abdulmuizpk@gmail.com>
2025-07-03 03:01:58 +0500

    .

--- rm_bank_apps/accounts/serializers.py
+++ rm_bank_apps/accounts/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Account
 
-class AccountSerialier(serializers.ModelSerializer):
+class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
         fields = ['id', 'user', 'account_number', 'balance', 'created_at']
--- rm_bank_apps/users/views.py
+++ rm_bank_apps/users/views.py
@@ -2,7 +2,9 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework import status
+from django.db import transaction
 from .serializers import UserSerializer
+from ..accounts.serializers import AccountSerializer
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -40,4 +42,37 @@ def change_password(request):
         return Response({'error': 'Old password is incorrect'}, status=400)
     user.set_password(new_password)
     user.save()
-    return Response() 
+    return Response({'message': 'Password changed successfully'})
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+@transaction.atomic
+def register_customer_with_account(request):
+    user_data = request.data.get('user')
+    account_data = request.data.get('account')
+
+    if not user_data or not account_data:
+        return Response({'error': 'Both user and account data are required'}, status=400)
+
+    user_data['roles'] = ['account_holder']
+
+    user_serializer = UserSerializer(data=user_data)
+    if user_serializer.is_valid():
+        saved_user = user_serializer.save()
+
+        account_data['user'] = saved_user.id
+        account_serializer = AccountSerializer(data=account_data)
+
+        if account_serializer.is_valid():
+            saved_account = account_serializer.save()
+
+            return Response({
+                'user': UserSerializer(saved_user).data,
+                'account': AccountSerializer(saved_account).data,
+                'message': 'User and account created successfully'
+            }, status=201)
+        else:
+            return Response(account_serializer.errors, status=400)
+    else:
+        return Response(user_serializer.errors, status=400)
+        
